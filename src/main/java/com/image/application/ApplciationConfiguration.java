df998f97813b3b7d12d6f09a6c386c1dd4f41e9b
nitesh <sksnitesh@gmail.com>
2016-10-05 18:39:24 +0530

    image upload service is created

--- src/main/java/com/image/application/ApplciationConfiguration.java
+++ src/main/java/com/image/application/ApplciationConfiguration.java
@@ -9,34 +9,16 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 /**
  * @author Nitesh
  */
 @Configuration
 @EnableWebMvc
-@EnableSwagger2
 @ComponentScan(basePackages = "com.image")
 public class ApplciationConfiguration extends WebMvcConfigurerAdapter {
 
-	/**
-	 * Swagger configuration
-	 * 
-	 * @return
-	 */
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
-	}
-
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
